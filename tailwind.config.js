716e4ee9a733ec048a0bf415ffcf2b2614da1003
stranger0g <135604372+stranger0g@users.noreply.github.com>
2025-04-11 14:30:45 +0300

    feat: Initial project setup with Vite, React, TS, Tailwind v3.4

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // All relevant files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
